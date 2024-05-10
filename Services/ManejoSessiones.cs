d09f353729f95781eb6c2d5d74089c04ae212352
Bryan Castillo <jimbrayan@hotmail.com>
2024-05-10 11:17:08 -0300

    manejosession

---
+++ Services/ManejoSessiones.cs
@@ -0,0 +1,32 @@
+﻿using BEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ManejoSessiones
+    {
+        static Usuario _sesion;
+
+        public static Usuario Session
+        {
+            get { return _sesion; }
+        }
+
+        public static void LogIn(Usuario usuario)
+        {
+            _sesion = usuario;
+        }
+        public static void LogOut()
+        {
+            _sesion = null;
+        }
+        public static bool IsLogged()
+        {
+            return Session!= null;
+        }
+    }
+}
