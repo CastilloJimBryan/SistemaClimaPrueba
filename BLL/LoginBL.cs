fe75ce1444dfeada59c59ce1439547c2272a9083
Bryan Castillo <jimbrayan@hotmail.com>
2024-05-10 12:05:20 -0300

    se hizo cambio login

---
+++ BLL/LoginBL.cs
@@ -0,0 +1,33 @@
+﻿using BEL;
+using DAL;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class LoginBL
+    {
+        UsuarioDAL usuarioDAL;
+
+        public LoginBL()
+        {
+            usuarioDAL= new UsuarioDAL();
+        }
+
+        public void Login(string text1,string text2)
+        {
+            if (String.IsNullOrEmpty(text1) || String.IsNullOrEmpty(text2)) throw new Exception("Campos Vacios!");
+            Usuario user=usuarioDAL.ObtenerNomYPass(text1,text2);
+            if (user == null) throw new Exception("Usuario y/o Contraseña Incorrecto");
+            ManejoSessiones.LogIn(user);
+        }
+        public void LogOut()
+        {
+            ManejoSessiones.LogOut();
+        }
+    }
+}
--- DAL/UsuarioDAL.cs
+++ DAL/UsuarioDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BEL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,29 @@ namespace DAL
 {
     public class UsuarioDAL
     {
-
+        public void ValidarFilas(Usuario u,DataRow dr)
+        {
+            u.UsuarioId = int.Parse(dr["UsuarioId"].ToString());
+            u.Nombre = dr["Nombre"].ToString();
+            u.Apellido = dr["Apellido"].ToString();
+            u.Pass = dr["Pass"].ToString();
+            u.Rol_Id = int.Parse(dr["Rol_Id"].ToString());
+        }
+       
+        public Usuario ObtenerNomYPass(string nom,string pass)
+        {
+            string sql = "SELECT UsuarioId,Nombre,Apellido,Pass,Rol FROM Usuario " +
+                " INNER JOIN Rol ON Rol.RolId= Usuario.Rol_Id " +
+                " WHERE Usuario.Nombre='" + nom + "' AND Pass='" + pass + "'";
+            DAO dAO = new DAO();
+            DataSet ds = dAO.Read(sql);
+            if(ds.Tables.Count > 0 && ds.Tables[0].Rows.Count>0)
+            {
+                Usuario u=new Usuario();
+                ValidarFilas(u, ds.Tables[0].Rows[0]);
+                return u;
+            }
+            return null;
+        }
     }
 }
